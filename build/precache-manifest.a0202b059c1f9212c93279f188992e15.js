a0338de6a1116a3be56edfa359bddfdeb9d73a90
Neil Hughes <enhughesiasm@googlemail.com>
2020-04-06 16:42:07 +0100

    Initial repo creation

---
+++ build/precache-manifest.a0202b059c1f9212c93279f188992e15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa777e76c711ff1e9a57858d0c9239e9",
+    "url": "/complex2/index.html"
+  },
+  {
+    "revision": "f2236137393183b385b7",
+    "url": "/complex2/static/css/2.0c251c65.chunk.css"
+  },
+  {
+    "revision": "4fb327009fc67fcaf287",
+    "url": "/complex2/static/css/main.c2089e65.chunk.css"
+  },
+  {
+    "revision": "f2236137393183b385b7",
+    "url": "/complex2/static/js/2.c205a64a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/complex2/static/js/2.c205a64a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb327009fc67fcaf287",
+    "url": "/complex2/static/js/main.3038955b.chunk.js"
+  },
+  {
+    "revision": "8456facb13d44747107c",
+    "url": "/complex2/static/js/runtime-main.0fbf9c9c.js"
+  }
+]);
---
+++ src/resources/styles/styles.js
@@ -0,0 +1,13 @@
+require('../../../node_modules/bulma/sass/utilities/initial-variables.sass');
+require('../../../node_modules/bulma/sass/utilities/functions.sass');
+
+// // alter bulma variables & load rest of bulma
+require('./customise.scss');
+
+// require('./bulma-slider.css');
+
+// // my responsive fixes
+// require('./responsive.scss');
+
+// // feelings specific
+require('./complex2.scss');
